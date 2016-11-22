d5ed37ef5d1099f7bc89b97db6f7fefc441ddf80
Sergey Arkhangelskiy <mc.vertix@gmail.com>
2016-11-22 15:49:25 -0800

    Extract DefaultPolicy class

--- policy/MyStrategy.py
+++ policy/MyStrategy.py
@@ -1,5 +1,6 @@
 import cPickle
 import os
+import random
 import re
 import sys
 import time
@@ -46,25 +47,21 @@ class QFunction(object):
 
     def Q(self, state):
         state = np.matmul(state, self.vars['model/hidden1/weights:0'])
-        # state += self.vars['model/hidden1/biases:0']
         state = BatchNorm(state, self.vars, 'model/hidden1/BatchNorm')
         state = ReLu(state)
 
         state = np.matmul(state, self.vars['model/hidden2/weights:0'])
-        # state += self.vars['model/hidden2/biases:0']
         state = BatchNorm(state, self.vars, 'model/hidden2/BatchNorm')
         state = ReLu(state)
 
         value = np.matmul(state, self.vars['model/val_hid/weights:0'])
         value = BatchNorm(value, self.vars, 'model/val_hid/BatchNorm')
-        # value += self.vars['model/val_hid/biases:0']
         value = ReLu(value)
         value = np.matmul(value, self.vars['model/value/weights:0'])
         value += self.vars['model/value/biases:0']
 
         adv = np.matmul(state, self.vars['model/adv_hid/weights:0'])
         adv = BatchNorm(adv, self.vars, 'model/adv_hid/BatchNorm')
-        # adv += self.vars['model/adv_hid/biases:0']
         adv = ReLu(adv)
         adv = np.matmul(adv, self.vars['model/advantage/weights:0'])
         adv += self.vars['model/advantage/biases:0']
@@ -75,6 +72,33 @@ class QFunction(object):
         value = self.Q(state.to_numpy())
         res = np.argmax(value)
         return res, value[res]
+
+
+class DefaultPolicy(object):
+    def __init__(self, lane):
+	self.lane = lane
+	self.target = 0
+	self.ticks_on_target = 0
+
+    def Act(self, state):
+	if state.my_state.hp < 50:
+            res = 0 + self.lane  # FLEE
+	    self.ticks_on_target = 100
+        elif state.enemy_states:
+	    if self.ticks_on_target > 50:
+		self.ticks_on_target = 0
+		self.target = random.choice(
+		    range(min(MAX_TARGETS_NUM, len(state.enemy_states))))
+            res = 6 + self.target   # RANGE ATTACK CLOSEST
+	    self.ticks_on_target += 1
+        else:
+	    self.ticks_on_target = 100
+            res = 3 + self.lane  # ADVANCE
+	print res
+	return res
+
+    def Stop(self):
+	pass
 
 
 class RemotePolicy(object):
@@ -116,29 +140,31 @@ class RemotePolicy(object):
         epsilon = 0.5 / (1 + self.steps / 1000.)
         self.steps += 1
 
-        if np.random.rand() < epsilon:
+        if np.random.rand() < epsilon or self.q is None:
             return np.random.choice(range(self.max_actions))
 
-        if self.q == None: # or np.random.rand() < 0.5:
-            if state.my_state.hp < 50:
-                # print 'FLEE'
-                return 0  # FLEE
-            elif state.enemy_states:
-                return 2  # RANGE ATTACK CLOSEST
-            else:
-                # print 'ADVANCE'
-                return 1  # ADVANCE
-        else:
-            res, val = self.q.Select(state)
-            action = (['FLEE', 'ADVANCE'] +
-                      ['ATTACK_%d' %i for i in range(1, MAX_TARGETS_NUM + 1)])[res]
-            if action != self.last_action:
-                self.last_action = action
-                print '%s: %.2f' % (action, val)
-            return res
+        # if self.q == None: # or np.random.rand() < 0.5:
+        #     if state.my_state.hp < 50:
+        #         # print 'FLEE'
+        #         return 0  # FLEE
+        #     elif state.enemy_states:
+        #         return 2  # RANGE ATTACK CLOSEST
+        #     else:
+        #         # print 'ADVANCE'
+        #         return 1  # ADVANCE
+        # else:
+        res, val = self.q.Select(state)
+        action = (['FLEE_%s' % ln for ln in ['TOP', 'MIDDLE', 'BOTTOM']] +
+                  ['ADVANCE_%s' % ln for ln in ['TOP', 'MIDDLE', 'BOTTOM']] +
+                  ['ATTACK_%d' %i for i in range(1, MAX_TARGETS_NUM + 1)])[res]
+        if action != self.last_action:
+            self.last_action = action
+            print '%s: %.2f' % (action, val)
+        return res
 
 
-NUM_ACTIONS = 2 + MAX_TARGETS_NUM
+NUM_ACTIONS = 6 + MAX_TARGETS_NUM
+LANES = [LaneType.TOP, LaneType.MIDDLE, LaneType.BOTTOM]
 
 class MyStrategy:
     def __init__(self):
@@ -149,10 +175,10 @@ class MyStrategy:
         else:
             self.sock = None
 
-        if len(sys.argv) > 2 and zmq:
+        if len(sys.argv) > 2 and sys.argv[2] and sys.argv[2] != '0' and zmq:
             self.policy = RemotePolicy(sys.argv[2], NUM_ACTIONS)
         else:
-            self.policy = RemotePolicy(None, NUM_ACTIONS)
+            self.policy = DefaultPolicy(random.choice(LANES))
 
         self.last_score = 0.
         self.initialized = False
@@ -162,11 +188,11 @@ class MyStrategy:
 
         self.exp = {'s':[], 'a': [], 'r': [], 's1': []}
         self.next_file_index = 0
-        self.flee_action = None
-        self.advance_action = None
-        self.lane = None
+        self.flee_actions = []
+        self.advance_actions = []
 
     def stop(self):
+        self.SaveExperience(self.last_state, self.last_action, 0, None)
         self.policy.Stop()
 
     def SaveExperience(self, s, a, r, s1):
@@ -177,7 +203,7 @@ class MyStrategy:
             's': s.to_numpy(),
             'a': a,
             'r': r,
-            's1': s1.to_numpy()
+            's1': s1.to_numpy() if s1 else None
         }
         self.sock.send_pyobj(data)
         if self.sock.recv() != "Ok":
@@ -190,22 +216,21 @@ class MyStrategy:
         @type game: Game
         @type move: Move
         """
-        if self.flee_action is None:
-            if len(sys.argv) > 3:
-                self.lane = int(sys.argv[3])
-            else:
-                self.lane = LaneType.TOP
-            self.flee_action = Actions.FleeAction(game.map_size, self.lane)
-            self.advance_action = Actions.AdvanceAction(game.map_size, self.lane)
-
-        lane = self.lane
+        if not self.flee_actions:
+            for lane in LANES:
+                self.flee_actions.append(Actions.FleeAction(game.map_size, lane))
+                self.advance_actions.append(Actions.AdvanceAction(game.map_size, lane))
 
         state = State.WorldState(me, world, game)
+        noop = Actions.NoOpAction()
 
         targets = [enemy.unit for enemy in state.enemy_states][:MAX_TARGETS_NUM]
-        actions = ([self.flee_action, self.advance_action] +
-                   [Actions.RangedAttack(game.map_size, lane, t) for t in targets] +
-                   [self.advance_action] * (MAX_TARGETS_NUM - len(targets)))
+        actions = (self.flee_actions + self.advance_actions +
+                   [Actions.RangedAttack(
+                       game.map_size,
+                       random.choice([LaneType.TOP, LaneType.MIDDLE, LaneType.BOTTOM]), t)
+                    for t in targets] +
+                   [noop] * (MAX_TARGETS_NUM - len(targets)))
 
         a = self.policy.Act(state)
         reward = world.get_my_player().score - self.last_score
